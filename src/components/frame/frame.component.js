3c9fbddbbf900b05ebee96b736fda8e4d4eccabf
Tudor Gălățan <tudorgalatan@gmail.com>
2020-02-08 19:57:15 +0200

    Added the components files.
    I added the files regarding the componenents.

---
+++ src/components/frame/frame.component.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import 'frame.component.scss';
+
+
+
+class Frame extends Component
+{
+    render ()
+    {
+        return (
+            
+        );
+    }
+}
